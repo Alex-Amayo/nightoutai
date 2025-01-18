1219fe2b13f8f5ee8540235373ecdfad825091b3
alex-amayo <alexjamayo@gmail.com>
2025-01-18 11:49:13 -0800

    About page fix

--- app/tabs/(tabs)/about.tsx
+++ app/tabs/(tabs)/about.tsx
@@ -15,7 +15,7 @@ const AboutPage = () => {
     <View style={{ flex: 1 }}>
       <ScrollView
         contentContainerStyle={{
-          ...styles.screen,
+          ...styles.scrollview,
           // Configure background color with theme
           backgroundColor: theme.values.backgroundColor,
         }}>
@@ -29,9 +29,6 @@ const AboutPage = () => {
             {'\n'}
             It features an AI chat (currently in development), where the AI is fully aware of
             restaurant options around you and helps explore personalized recommendations.{'\n'}
-            {'\n'}
-            This can be particularly useful if your in a new city or just looking to explore fresh
-            options.
           </StyledText>
         </View>
       </ScrollView>
@@ -43,18 +40,18 @@ const AboutPage = () => {
 export default AboutPage;
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
+  scrollview: {
     alignItems: 'center',
     justifyContent: 'center',
-    padding: '3%',
-    paddingTop: 100,
-    paddingBottom: 100,
+    paddingVertical: 20,
+    height: '100%',
+    paddingHorizontal: '3%',
   },
   container: {
     flex: 1,
     gap: 10,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 20,
   },
 });
